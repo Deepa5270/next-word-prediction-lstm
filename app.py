d13042011a48b3184aa50ad9f92c60ec98f5ffb7
Deepa5270 <deepa7932praj@gmail.com>
2026-06-23 14:04:57 +0530

    Use h5 model

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 #Load the LSTM Model
-model = load_model('next_word_lstm.keras')
+model = load_model('next_word_lstm.h5', compile=False)
+
 
 
 #3 Laod the tokenizer
